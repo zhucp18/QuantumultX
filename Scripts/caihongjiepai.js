0e32228e593cf9ddb9d2c7982d46b9e5c0268f2d
穹顶之下 <39182794+zhucp18@users.noreply.github.com>
2021-10-31 18:35:49 +0800

    Update caihongjiepai.js

--- Scripts/caihongjiepai.js
+++ Scripts/caihongjiepai.js
@@ -10,7 +10,7 @@ const headers = {
 'Accept' : `application/json, text/javascript, */*; q=0.01`,
 'Accept-Encoding' : `gzip, deflate`,
 'Origin' : `http://caihongjiepai.me`,
-'Cookie' : `username_4eb3014d=zhucp18; token_4eb3014d=4e1faa27a619e16ab0aeea093b439af68412784277d9e21102fa1994bc1c6c8b1635607261; addinfo=%7B%22chkadmin%22%3A1%2C%22chkarticle%22%3A1%2C%22levelname%22%3A%22%5Cu534f%5Cu4f5c%5Cu8005%22%2C%22userid%22%3A%224486%22%2C%22useralias%22%3A%22zhucp18%22%7D; PHPSESSID=q2tfchd0o3dkuvmau7fui8cm32; cookie_islog=1; is_mochu_us_load=mochu_us; timezone=8; mochu_us_notice_alert=1`,
+'Cookie' : `timezone=8; PHPSESSID=mpn4n9ubl94ggc82mq86g8djav; username_4eb3014d=zhucp18; token_4eb3014d=3d3c02c8b4b9da9084e4dee0cc8c734935e99ac694bd907e6017e74610e8826d1638096698; addinfo=%7B%22chkadmin%22%3A1%2C%22chkarticle%22%3A1%2C%22levelname%22%3A%22%5Cu534f%5Cu4f5c%5Cu8005%22%2C%22userid%22%3A%224486%22%2C%22useralias%22%3A%22zhucp18%22%7D; searchbetter=0; cookie_islog=1; is_mochu_us_load=mochu_us; mochu_us_notice_alert=1`,
 'Connection' : `keep-alive`,
 'Host' : `caihongjiepai.me`,
 'User-Agent' : `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/93 Version/11.1.1 Safari/605.1.15`,
